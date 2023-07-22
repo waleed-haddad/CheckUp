c38ce3ef0e0b1e0a5bc1f525a68cb174ed99b79f
Waleed <waleedsuhail29@gmail.com>
2023-07-22 12:22:07 -0400

    First commit to project.

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, useWindowDimensions, TextInput, Pressable } from 'react-native';
+import styles from './styles';
+import Svg, {Image, Ellipse, ClipPath} from 'react-native-svg';
+import Animated, { useSharedValue, useAnimatedStyle, interpolate, withTiming, withDelay, withSequence, withSpring } from 'react-native-reanimated';
+
+// Change the X from a text with onpress -> Pressable with onpress
 
 export default function App() {
+  const { height, width } = useWindowDimensions();
+  const imagePosition = useSharedValue(1);
+  const fomrBUttonScale = useSharedValue(1);
+  const [isRegistering, setIsRegistering] = useState(false);
+
+
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(imagePosition.value, [0, 1], [-height /2, 0])
+    return {
+      transform: [{translateY: withTiming(interpolation, {duration: 1000})}]
+    }
+  })
+
+  const buttonsAnimatedStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(imagePosition.value, [0, 1], [250, 0])
+    return {
+      opacity: withTiming(imagePosition.value, {duration: 500}),
+      transform: [{translateY: withTiming(interpolation, {duration: 1000})}]
+    };
+  });
+
+  const closeButtonContainerStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(imagePosition.value, [0, 1], [180, 360])
+    return {
+      opacity: withTiming(imagePosition.value === 1 ? 0 : 1, {duration: 800}),
+      transform: [{rotate: withTiming(interpolation + "deg", {duration: 1000})}]
+    }
+  })
+
+  const formAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: imagePosition.value === 0 ? withDelay(400, withTiming(1, {duration: 800})) : withTiming(0, {duration: 300}),
+    }
+  })
+
+  const formButtonAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: fomrBUttonScale.value}]
+    }
+  })
+
+  const loginHandler = () => {
+    imagePosition.value = 0;
+    if (isRegistering) {
+      setIsRegistering(false);
+    }
+  }
+
+  const registerHandler = () => {
+    imagePosition.value = 0;
+    if (!(isRegistering)) {
+      setIsRegistering(true);
+    }
+  }  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[StyleSheet.absoluteFill, imageAnimatedStyle]}>
+        <Svg height={height + 35} width={width}>
+          <ClipPath id="clipPathId">
+            <Ellipse cx={width/2} rx={height} ry={height + 35}/>
+          </ClipPath>
+          <Image 
+            href={require('./assets/login-background-4.jpg')} 
+            width={width} 
+            height={height + 35}
+            preserveAspectRatio='xMidYMid slice'
+            clipPath="url(#clipPathId)"
+          />
+        </Svg>
+        <Animated.View style={[styles.closeButtonContainer, closeButtonContainerStyle]}>
+          <Pressable onPress={() => imagePosition.value = 1}>
+            <Text>X</Text>
+          </Pressable>
+        </Animated.View>
+      </Animated.View>
+      <View style={styles.bottomContainer}>
+        <Animated.View style={buttonsAnimatedStyle}>
+          <Pressable style={styles.button} onPress={loginHandler}>
+            <Text style={styles.buttonText}>Login</Text>
+          </Pressable>
+        </Animated.View>
+        <Animated.View style={buttonsAnimatedStyle}>
+          <Pressable style={styles.button} onPress={registerHandler}>
+            <Text style={styles.buttonText}>Register</Text>
+          </Pressable>
+        </Animated.View>
+        <Animated.View style={[styles.formInputContainer, formAnimatedStyle]}>
+          <TextInput placeholder="Email" placeholderTextColor="black" style={styles.textInput}/>
+          {isRegistering && (
+            <TextInput placeholder="Full Name" placeholderTextColor="black" style={styles.textInput}/>
+          )}
+          <TextInput placeholder="Password" placeholderTextColor="black" style={styles.textInput}/>
+          <Animated.View style={[styles.formButton, formButtonAnimatedStyle]}>
+            <Pressable onPress={() => fomrBUttonScale.value = withSequence(withSpring(1.5), withSpring(1))}>
+              <Text style={styles.buttonText}>
+                {isRegistering ? 'Register' : 'Login'}
+              </Text>
+            </Pressable>
+          </Animated.View>
+        </Animated.View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
