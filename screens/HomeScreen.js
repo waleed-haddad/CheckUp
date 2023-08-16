d445317bfebb008da7094534c42136684d5d7296
Waleed <waleedsuhail29@gmail.com>
2023-08-16 18:02:29 -0400

    Notification functionality implemented, only button-press for now.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,14 +6,45 @@ import Animated, { useSharedValue, useAnimatedStyle, interpolate, withTiming, wi
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
+import * as Notifications from 'expo-notifications';
+
+//First, set the handler that will cause the notification
+//to show the alert
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.homescreencontainer}>
       <Text>HomeScreen</Text>
       <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout"/>
+      <Button onPress={async () => {
+          await schedulePushNotification();
+        }} title="notification"
+      /> 
     </View>
   )
 }
+
+// Second, call the method
+async function schedulePushNotification() {
+  Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Look at that notification',
+      body: "I'm so proud of myself!",
+    },
+    trigger: { 
+      seconds: 60, repeats: false},
+  });
+  Notifications.cancelAllScheduledNotificationsAsync();
+  Notifications.
+}
+
 
 export default HomeScreen
