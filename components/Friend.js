0c44998be5f1954fb47fa99c6396796d6c5c24eb
Waleed <waleed.haddad@mail.utoronto.ca>
2023-08-25 23:48:41 -0400

    Added 'Friend' component which will be used to create multiple objects for Friend List.

---
+++ components/Friend.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Touchable, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet } from 'react-native';
+
+const Friend = (props) => {
+
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.Text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '##F6793F',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText: {
+        maxWidth: '80%',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#F6793F',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+});
+
+export default Friend;
