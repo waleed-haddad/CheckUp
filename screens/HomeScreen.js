cb64a6566c28504adb0775d0a104eddbad8688b7
Waleed <waleed.haddad@mail.utoronto.ca>
2023-09-04 21:10:20 -0400

    Added the logic code to the homescreen page, bug fixes needed but the base idea works!

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,7 +7,7 @@ import { FIREBASE_AUTH } from '../FirebaseConfig';
 import * as Notifications from 'expo-notifications';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Modal from "react-native-modal";
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 
 const HomeScreen = ({ navigation }) => {
   // These states handle individual and lists of friends
@@ -18,7 +18,10 @@ const HomeScreen = ({ navigation }) => {
   const handleAddFriend = () => {
     Keyboard.dismiss()
     setFriendItems([...friendItems, friend])
+    const tempName = friend;
     setFriend(null);
+    handleAddDate(tempName);
+    toggleModal();
   }
   const completeFriend = (index) => {
     let itemsCopy = [...friendItems];
@@ -45,40 +48,69 @@ const HomeScreen = ({ navigation }) => {
     setDate(currentDate);
   };
 
-  const handleAddDate = () => {
-    setFriendDate([...friendDate, date])
+  const handleAddDate = (tempName) => {
+    setFriendDate([...friendDate, date]);
+    NotificationPress(date, tempName);
   }
 
   //Handle interval picking
   const [selectedLanguage, setSelectedLanguage] = useState();
 
+  //Handle notifications
+
+  //First, set the handler that will cause the notification
+  //to show the alert
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+  //listens to incoming notification, different actions for one-time and recurring notifications
+  // React.useEffect(() => {
+  //   const subscription = Notifications.addNotificationReceivedListener(notification => {
+  //     if (notification.request.trigger.repeats) {
+  //       console.log("recurring noti")
+  //     } else {
+  //       scheduleIntervalNotification(1);
+  //     }
+  //   });
+  //   return () => subscription.remove();
+  // }, []);
+
+  const NotificationPress = async (dateNow, tempName) => {
+    //oldDate used to account for time shift
+    const oldDate = new Date();
+    await scheduleOneTimeNotification(dateNow.getTime()/1000, oldDate.getTime()/1000, tempName);
+  }
+
   return (
     <View style={homescreen_styles.container}>
       {/* Friends List */}
       <View style={homescreen_styles.friendsWrapper}>
-      <Modal isVisible={modalVisible}>
-        <View style={homescreen_styles.modalContainer}>
-          <Text style={homescreen_styles.modalTitle}>Pick your start date and notification time!</Text>
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            mode={mode}
-            is24Hour={false}
-            onChange={onChange}
-            display='inline'
-            themeVariant="light"
-          />
-          <Pressable style={homescreen_styles.addFriendContainer} onPress={() => {
-            handleAddFriend();
-            handleAddDate();
-            toggleModal();
-          }
-                                                                                }>
-            <Text style={homescreen_styles.modalTitle}>Add Friend</Text>
-          </Pressable>
-        </View>
-      </Modal>
-      {/* <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout"/> */}
+        <Modal isVisible={modalVisible}>
+          <View style={homescreen_styles.modalContainer}>
+            <Text style={homescreen_styles.modalTitle}>Pick your start date and notification time!</Text>
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={mode}
+              is24Hour={false}
+              onChange={onChange}
+              display='inline'
+              themeVariant="light"
+            />
+            <Pressable style={homescreen_styles.addFriendContainer} onPress={() => {
+              handleAddFriend();
+            }
+            }>
+              <Text style={homescreen_styles.modalTitle}>Add Friend</Text>
+            </Pressable>
+          </View>
+        </Modal>
+        {/* <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout"/> */}
         <Text style={homescreen_styles.sectionTitle}>Friends List</Text>
         <View style={homescreen_styles.items}>
           {/* This is where the friend list will go! */}
@@ -86,7 +118,7 @@ const HomeScreen = ({ navigation }) => {
             friendItems.map((item, index) => {
               return (
                 <TouchableOpacity key={index} onPress={() => completeFriend(index)}>
-                  <Friend Text={item} Day={friendDate[index].getDay()} Time={friendDate[index].toLocaleTimeString()}/>
+                  <Friend Text={item} Day={friendDate[index].getDay()} Time={friendDate[index].toLocaleTimeString()} />
                 </TouchableOpacity>
               )
             })
@@ -112,6 +144,32 @@ const HomeScreen = ({ navigation }) => {
   )
 }
 
+// One-time notification, scheduled based on user's selected date and time
+async function scheduleOneTimeNotification(date, oldDate, tempName) {
+  const initalNotification = await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Hey! Have you spoken to ' + tempName + " recently?",
+      body: "This is your reminder to check up on them :)",
+    },
+    trigger: { 
+      seconds: date - oldDate, repeats: false
+    },
+  });
+}
+
+// Repeating notification, scheduled based on user's selected interval range (in weeks)
+async function scheduleIntervalNotification(weeks) {
+  const initalNotification = await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'This is a repeating notification',
+      body: "I'm so proud of myself!",
+    },
+    trigger: { 
+      seconds: 60, repeats: true
+    },
+  });
+}
+
 const homescreen_styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -134,7 +192,7 @@ const homescreen_styles = StyleSheet.create({
     bottom: 60,
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-around', 
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
   input: {
@@ -160,7 +218,7 @@ const homescreen_styles = StyleSheet.create({
   dateTime: {
     marginTop: 30,
     marginBottom: 30,
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
   },
   modalContainer: {
     backgroundColor: "#FEF7E5",
